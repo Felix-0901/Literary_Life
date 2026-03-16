20a0af9b171d2c6ff4b4c58f2f021329d8637fd7
Felix <lmx0901260128@gmail.com>
2026-03-16 11:36:12 +0800

    feat(web): 添加 Web 端域名至 CORS 允許名單

--- literary_life_backend/app/config.py
+++ literary_life_backend/app/config.py
@@ -10,7 +10,8 @@ class Settings(BaseSettings):
     # Database
     DATABASE_URL: str = "postgresql+psycopg://literary_life:literary_life_dev@db:5432/literary_life"
     BACKEND_CORS_ORIGINS: str = (
-        "http://localhost:3000,http://127.0.0.1:3000"
+        "http://localhost:3000,http://127.0.0.1:3000,"
+        "https://literaryweb.beioverworked.com"
     )
 
     # JWT
